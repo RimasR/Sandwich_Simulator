e343466c0ea620694fc0e7129ad8a576f2b0627e
Rimantas Radziunas <radziunas.rim@gmail.com>
2017-03-08 18:31:51 +0200

    initial

---
+++ SandwichSimulator/SandwichSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SandwichSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
